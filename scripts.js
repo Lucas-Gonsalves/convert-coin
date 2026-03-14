7089974c0e9b0ce9652ab5e9b4ef4ab1ab3dc503
Lucas Gonçalves <lucasluz1710@gmail.com>
2026-03-14 11:34:24 -0300

    feat(scripts): displaying description   A function has been created to transform a number into its value in Brazilian Real. The function is structured to perform the conversion and dynamically display the values described on the screen.

--- scripts.js
+++ scripts.js
@@ -6,15 +6,44 @@ const GBP = 7.06
 
 // Obtaining html elements
 const form = document.querySelector('form')
-const footer = document.querySelector('main footer')
 const amountInput = document.getElementById('amount')
 const currencySelector = document.getElementById('currency')
+const footer = document.querySelector('main footer')
+const descriptionSpan = document.getElementById('description')
 
 // Manipulationg the input amount to receive only numbers
 amountInput.addEventListener('input', () => {
   const hasCharactersRegex = /\D+/g
   amountInput.value = amountInput.value.replace(hasCharactersRegex, '');
 })
+
+// Format the coin to Real brazilian
+function formatCurrencyBRL(value) {
+  return Number(value).toLocaleString('pt-BR', {
+    style: 'currency',
+    currency: 'BRL'
+  })
+}
+
+// Function to convert the coin
+function convertCurrency(amount, price, symbol) {
+  const priceFormated = formatCurrencyBRL(price)
+
+  try {
+    // Displaying the quotation of the coin selected
+    descriptionSpan.textContent = `${symbol} 1 = ${priceFormated}`
+
+    // Applies the class that displays the footer to show the result
+    footer.classList.add('show-result')
+  
+  } catch (error) {
+    // Remove the class that displays the footer oculting he
+    footer.classList.remove('show-result')
+
+    console.log(error)
+    alert('We were unable to convert. Please try again later.')
+  }
+}
 
 // Capturing submit event of the form 
 form.onsubmit = (event) => {
@@ -33,9 +62,4 @@ form.onsubmit = (event) => {
       convertCurrency(amountInput.value, GBP, '£')
       break
   }
-}
-
-// Function to convert the coin
-function convertCurrency(amount, price, symbol) {
-  console.log(amount, price, symbol)
 }
