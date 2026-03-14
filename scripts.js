e5ff9c7e04e82b995fade0902f68a844d5704802
Lucas Gonçalves <lucasluz1710@gmail.com>
2026-03-14 11:20:55 -0300

    feat(scripts): create convert structure   Capturing the main HTML components to perform currency conversion. It will be possible to convert to USD, EUR, and GBP.

--- scripts.js
+++ scripts.js
@@ -1,7 +1,41 @@
-const inputAmount = document.getElementById('amount')
+// Quotation coins of the day
+const USD = 5.33
+const EUR = 6.05
+const GBP = 7.06
 
-// Manipulationg the input amoun to receive only numbers
-inputAmount.addEventListener('input', () => {
+
+// Obtaining html elements
+const form = document.querySelector('form')
+const footer = document.querySelector('main footer')
+const amountInput = document.getElementById('amount')
+const currencySelector = document.getElementById('currency')
+
+// Manipulationg the input amount to receive only numbers
+amountInput.addEventListener('input', () => {
   const hasCharactersRegex = /\D+/g
-  inputAmount.value = inputAmount.value.replace(hasCharactersRegex, '');
+  amountInput.value = amountInput.value.replace(hasCharactersRegex, '');
 })
+
+// Capturing submit event of the form 
+form.onsubmit = (event) => {
+  event.preventDefault()
+
+  switch(currencySelector.value) {
+    case 'USD':
+      convertCurrency(amountInput.value, USD, 'US$')
+      break
+   
+    case 'EUR':
+      convertCurrency(amountInput.value, EUR, '€')
+      break
+   
+    case 'GBP':
+      convertCurrency(amountInput.value, GBP, '£')
+      break
+  }
+}
+
+// Function to convert the coin
+function convertCurrency(amount, price, symbol) {
+  console.log(amount, price, symbol)
+}
